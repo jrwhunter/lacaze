5661a9e9fd3bc3a7e9f642915b41ec315e2cd777
jrwhunter <j.hunter@abdn.ac.uk>
2013-03-15 09:05:47 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lacaze::Application.config.secret_token = 'aebe012ec4add9f8272e1484f38c8cf8294a2b7a6956e3e671ab1e5c7cdb7fb3ca0cef24a2ac91d775accdb0969565e019a691562744fbdb01f14d066c00e14f'
